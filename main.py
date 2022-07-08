f770a9e1199afb211ac62e52b81d8ad11f461a5f
dhruv50ae <linkdp2012@gmail.com>
2022-07-08 15:51:10 +0100

    Prepare the question push logic

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-class User:
-    def __init__(self, userId, username):
-        self.id = userId
-        self.username = username
-        self.following = 0
-        self.followers = 0
-    def follow(self, user):
-        user.followers += 1
-        self.following +=1
+from question_model import Question
+from data import question_data
 
-user1 = User('1', 'Salad')
-user2 = User('2', "Sauce")
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
-user1.follow(user2)
-print(user1.followers)
-print(user1.following)
-print(user2.followers)
-print(user2.following)
+print(question_bank)
