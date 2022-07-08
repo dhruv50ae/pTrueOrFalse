fd6098e0cc19bab3b5d80f75c308e5caa6910e9c
dhruv50ae <linkdp2012@gmail.com>
2022-07-08 15:38:34 +0100

    set up final project

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ class User:
     def __init__(self, userId, username):
         self.id = userId
         self.username = username
+        self.following = 0
         self.followers = 0
+    def follow(self, user):
+        user.followers += 1
+        self.following +=1
 
-user1 = User('2', 'Salad sauce')
+user1 = User('1', 'Salad')
+user2 = User('2', "Sauce")
 
-print(user1.username)
+user1.follow(user2)
+print(user1.followers)
+print(user1.following)
+print(user2.followers)
+print(user2.following)
